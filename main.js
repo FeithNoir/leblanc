5e7cd6a2b8ca45fff7d43171249ede92981c6acd
Feith Noir <feithnoir@gmail.com>
2025-04-14 13:55:02 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                violet: '#7e22ce',
+                cyan: '#22d3ee',
+                pink: '#ec4899',
+                lime: '#a3e635',
+                darkgrey: '#1f2937',
+            },
+            fontFamily: {
+                sans: ['Montserrat', 'sans-serif'],
+                display: ['Playfair Display', 'serif'],
+            },
+        }
+    }
+}
